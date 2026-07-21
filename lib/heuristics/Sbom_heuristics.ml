8b4df3773c67a57c6ca17b2f73f929138cfd1c4a
Martin Jambon <martin@mjambon.com>
2026-07-20 23:01:23 -0700

    Remove useless files

--- lib/heuristics/Sbom_heuristics.ml
+++
@@ -1,5 +0,0 @@
-module Scan_file_tree = Scan_file_tree
-module Git_submodules = Git_submodules
-module License_files = License_files
-module Vendored_dirs = Vendored_dirs
-module Dune_vendored_dirs = Dune_vendored_dirs
